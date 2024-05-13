55ecafbcd6edd8c26b105a165666b6503f637fbc
woohyeok1000 <1000.woohyeok@gmail.com>
2024-05-13 11:41:47 +0900

    Feat: Add camerasetting, wallcollision
    - 캐릭터를 따라가는 카메라 움직임 추가
    - 벽에 tile collider 추가
    - 다음 작업을 위해 씬 한개 추가

--- Assets/Scripts/Behavior/HaniAimRotation.cs
+++ Assets/Scripts/Behavior/HaniAimRotation.cs
@@ -2,11 +2,10 @@
 using UnityEngine;
 
 public class HaniAimRotation : MonoBehaviour
-{
-    [SerializeField] private SpriteRenderer armRenderer;
-    [SerializeField] private Transform armPivot;
+{    
+    [SerializeField] private Transform bodyPivot;
 
-    [SerializeField] private SpriteRenderer characterRenderer;
+    [SerializeField] private SpriteRenderer HaniRenderer;
 
     private MainController controller;
 
@@ -29,8 +28,8 @@ public class HaniAimRotation : MonoBehaviour
     {
         float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
+        HaniRenderer.flipX = Mathf.Abs(rotZ) > 90f;
 
-        armPivot.rotation = Quaternion.Euler(0, 0, rotZ);
+        bodyPivot.rotation = Quaternion.Euler(0, 0, rotZ);
     }
 }
